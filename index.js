3e865c340a455dd52efcf840a3b5c65a6a2b7157
ito jun <tabero443@gmail.com>
2013-01-29 09:52:24 +0900

    課題３

---
+++ index.js
@@ -0,0 +1,22 @@
+﻿enchant(); //…①
+var picNumber = 0; //…②
+window.onload = function() { //…③
+var game = new Game(320, 480); //…④
+game.preload('http://enchantjs.com/assets/images/chara3.gif'); //…⑤
+game.onload = function() { //…⑥
+var bear = new Sprite(96, 96); //…⑦
+bear.image = //…⑧
+game.assets['http://enchantjs.com/assets/images/chara3.gif'];
+bear.frame = 0; //…⑨
+bear.addEventListener(Event.ENTER_FRAME, function() { //…⑩
+picNumber++; //…⑪
+if (picNumber > 3) { //…⑫
+picNumber = 0;
+}
+bear.frame = picNumber; //…⑬
+bear.x += 2; //…⑭
+});
+game.rootScene.addChild(bear); //…⑮
+};
+game.start(); //…⑯
+};
